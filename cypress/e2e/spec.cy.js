ad002f8a39aab81709b3c14c74ef689050479a87
p-baptista <phfb.br@gmail.com>
2024-07-03 09:53:33 -0300

    Added tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+const { it } = require("mocha");
+
 describe('template spec', () => {
   it('Verifica se app está abrindo', () => {
     cy.visit('http://127.0.0.1:7001/')
@@ -55,5 +57,54 @@ describe('template spec', () => {
     cy.contains('All').click();
     cy.get('.todo-list li')
       .should('have.length', 2);
+  });
+
+  it('Delete completed tasks', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.get('.clear-completed').click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
+
+  it('Complete all and get active tasks', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.toggle-all-label').click();
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
+
+  it('Complete all and get completed tasks', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.toggle-all-label').click();
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2)
+      .first()
+      .should('have.text', 'TP2 de ES');
   });
 });
